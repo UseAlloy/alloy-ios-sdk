f9fc31ab926c2bc97949b965db9ed4ca45b595e2
Mario Nachbaur <marionauta@users.noreply.github.com>
2020-04-15 12:15:03 +0200

    feat: change deployment target to iOS 11

--- Alloy/AlloyViewController.swift
+++ Alloy/AlloyViewController.swift
@@ -3,6 +3,6 @@ import UIKit
 public class AlloyViewController: UIViewController {
     public override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
+        view.backgroundColor = .white
     }
 }
