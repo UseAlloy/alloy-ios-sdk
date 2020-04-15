52ce28fc21afbb9351407669f4699c138c0238b3
Mario Nachbaur <marionauta@users.noreply.github.com>
2020-04-15 10:29:19 +0200

    Initial commit

---
+++ Alloy/AlloyViewController.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+class AlloyViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ AlloyExample/Alloy.swift
@@ -0,0 +1,11 @@
+//
+//  Alloy.swift
+//  Alloy
+//
+//  Created by Mario Nachbaur on 15/04/2020.
+//  Copyright © 2020 Alloy. All rights reserved.
+//
+
+class Alloy {
+
+}
