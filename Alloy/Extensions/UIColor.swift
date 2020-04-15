dbd165345731cf46e778818d2ff263c8e1e47379
Mario Nachbaur <marionauta@users.noreply.github.com>
2020-04-15 17:14:14 +0200

    feat(UIColor): create theme

---
+++ Alloy/Extensions/UIColor.swift
@@ -0,0 +1,9 @@
+import class UIKit.UIColor
+
+internal extension UIColor {
+    struct Theme {
+        static let black = UIColor.black
+        static let blue = UIColor(red: 0.29, green: 0.38, blue: 0.95, alpha: 1)
+        static let white = UIColor.white
+    }
+}
