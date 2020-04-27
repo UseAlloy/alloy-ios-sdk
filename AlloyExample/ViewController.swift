7b8f24c00ade1dcd66adcf8ac3996a5d5e86b995
Mario Nachbaur <marionauta@users.noreply.github.com>
2020-04-27 11:07:50 +0200

    feat(Example): add textfields to edit name

--- AlloyExample/ViewController.swift
+++ AlloyExample/ViewController.swift
@@ -2,6 +2,24 @@ import Alloy
 import UIKit
 
 class ViewController: UIViewController {
+    private lazy var nameFirstField: UITextField = {
+        let view = UITextField()
+        view.placeholder = "First name"
+        view.text = "John"
+        view.textAlignment = .center
+        view.layer.borderWidth = 1
+        return view
+    }()
+
+    private lazy var nameLastField: UITextField = {
+        let view = UITextField()
+        view.placeholder = "Last name"
+        view.text = "Doe"
+        view.textAlignment = .center
+        view.layer.borderWidth = 1
+        return view
+    }()
+
     let openButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("Open", for: .normal)
@@ -13,17 +31,34 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
 
+        view.addSubview(nameFirstField)
+        view.addSubview(nameLastField)
         view.addSubview(openButton)
+
+        nameFirstField.translatesAutoresizingMaskIntoConstraints = false
+        nameFirstField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100).isActive = true
+        nameFirstField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 40).isActive = true
+        nameFirstField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40).isActive = true
+
+        nameLastField.translatesAutoresizingMaskIntoConstraints = false
+        nameLastField.topAnchor.constraint(equalTo: nameFirstField.bottomAnchor, constant: 8).isActive = true
+        nameLastField.leadingAnchor.constraint(equalTo: nameFirstField.leadingAnchor).isActive = true
+        nameLastField.trailingAnchor.constraint(equalTo: nameFirstField.trailingAnchor).isActive = true
+
         openButton.translatesAutoresizingMaskIntoConstraints = false
-        openButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        openButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        openButton.topAnchor.constraint(equalTo: nameLastField.bottomAnchor, constant: 8).isActive = true
+        openButton.leadingAnchor.constraint(equalTo: nameLastField.leadingAnchor).isActive = true
+        openButton.trailingAnchor.constraint(equalTo: nameLastField.trailingAnchor).isActive = true
     }
 
     @objc private func openAlloy() {
         let config = AlloyConfig(
             token: ApiSecrets.token,
             secret: ApiSecrets.secret,
-            for: .new(AlloyEvaluationData(nameFirst: "John", nameLast: "Doe"))
+            for: .new(AlloyEvaluationData(
+                nameFirst: nameFirstField.text ?? "",
+                nameLast: nameLastField.text ?? ""
+            ))
         )
 
         let vc = AlloyViewController(with: config)
