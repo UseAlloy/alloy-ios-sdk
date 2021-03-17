ef4ce044d3fdb15cdf859894a79b81bf3bebfc7c
Mario Nachbaur <marionauta@users.noreply.github.com>
2021-03-17 17:04:51 +0100

    feat(NSBundle): correctly find bundle

--- Alloy/Extensions/NSBundle.swift
+++ Alloy/Extensions/NSBundle.swift
@@ -1,7 +1,16 @@
 import Foundation
 
 internal extension Bundle {
-    static var alloy: Bundle {
-        return Bundle(for: AlloyViewController.self)
-    }
+    static let alloy: Bundle = {
+        let myBundle = Bundle(for: AlloyViewController.self)
+
+        guard let resourceBundleURL = myBundle.url(
+            forResource: "AlloyAssets", withExtension: "bundle")
+            else { fatalError("AlloyAssets.bundle not found!") }
+
+        guard let resourceBundle = Bundle(url: resourceBundleURL)
+            else { fatalError("Cannot access AlloyAssets.bundle!") }
+
+        return resourceBundle
+    }()
 }
